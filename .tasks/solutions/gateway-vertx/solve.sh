5c79225f848ec24d52332e3b0229529e13396e6f
mcouliba <mcouliba@redhat.com>
2021-11-22 18:43:22 +0100

    Removed gateway-vertx

--- .tasks/solutions/gateway-vertx/solve.sh
+++
@@ -1,9 +0,0 @@
-##########################
-# gateway-vertx Solution #
-##########################
-
-DIRECTORY=`dirname $0`
-
-cp $DIRECTORY/*.java $DIRECTORY/../../../labs/gateway-vertx/src/main/java/com/redhat/cloudnative/gateway
-
-echo "Gateway Vertx Solved"
