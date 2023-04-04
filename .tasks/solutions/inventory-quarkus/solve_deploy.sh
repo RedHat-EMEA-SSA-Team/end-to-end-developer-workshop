31e1a246570d7b7fa25e25017b710fdab5e0c4cc
Alex Groom <alex.groom@gmail.com>
2023-04-04 11:16:54 +0100

    4.12 updates

--- .tasks/solutions/inventory-quarkus/solve_deploy.sh
+++ .tasks/solutions/inventory-quarkus/solve_deploy.sh
@@ -9,7 +9,7 @@ PROJECT_NAME=$1
 $DIRECTORY/solve.sh
 
 cd ${CONTEXT_FOLDER}
-mvn clean install -Dquarkus.kubernetes.deploy=true -DskipTests -Dcontainer-image.group=$(oc project -q)
+mvn clean install -Dquarkus.kubernetes.deploy=true -DskipTests -Dquarkus.container-image.group=$(oc project -q)
 
 oc label deployment inventory-coolstore app.openshift.io/runtime=quarkus --overwrite
 
