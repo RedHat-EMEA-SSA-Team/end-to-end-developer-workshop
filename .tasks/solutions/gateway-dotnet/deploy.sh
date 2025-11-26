9a37e5eb32e445c4a73276a9a24ad91946db333e
Alex Groom <agroom@redhat.com>
2025-11-26 11:38:05 +0000

    Up version to .net 9

--- .tasks/solutions/gateway-dotnet/deploy.sh
+++ .tasks/solutions/gateway-dotnet/deploy.sh
@@ -8,12 +8,7 @@ PROJECT_NAME=$1
 
 cd ${CONTEXT_FOLDER}
 
-#oc new-app dotnet:6.0~https://github.com/RedHat-EMEA-SSA-Team/end-to-end-developer-workshop#6.5 \
-#        --context-dir=labs/gateway-dotnet \
-#        --name=gateway-coolstore \
-#        --labels=app=coolstore,app.kubernetes.io/instance=gateway,app.kubernetes.io/part-of=coolstore,app.kubernetes.io/name=gateway,app.openshift.io/runtime=dotnet,component=gateway
-
-oc new-build dotnet:6.0 --name gateway-coolstore  \
+oc new-build dotnet:9.0 --name gateway-coolstore  \
   --labels=component=gateway \
   --env DOTNET_STARTUP_PROJECT=app.csproj --binary=true
 oc start-build gateway-coolstore --from-dir=. -w
